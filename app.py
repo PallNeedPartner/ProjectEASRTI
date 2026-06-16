a99c8acc1c291293b9fed1cc8100b7b5cc51b154
PallNeedPartner <naufaladitya4205@gmail.com>
2026-06-16 14:31:31 +0700

    add Dockerfile for HuggingFace Spaces

--- app.py
+++ app.py
@@ -161,5 +161,5 @@ def delete_history(id):
     conn.close()
     return redirect("/history")
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(debug=False, host="0.0.0.0", port=port)
